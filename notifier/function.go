b43720df80111c638515da787c0d838605e3f714
CRaLFa <minami.smd@gmail.com>
2025-11-17 10:18:26 +0900

    enable execution at previous day

--- notifier/function.go
+++ notifier/function.go
@@ -12,10 +12,12 @@ import (
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/google/uuid"
 	"github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
+	"github.com/samber/lo"
 )
 
 type parameters struct {
-	GarbageType string `json:"garbageType"`
+	GarbageType   string `json:"garbageType"`
+	IsPreviousDay bool   `json:"isPreviousDay"`
 }
 
 var jst = time.FixedZone("Asia/Tokyo", 9*60*60)
@@ -50,11 +52,17 @@ func postNotification(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	d := lo.Ternary(params.IsPreviousDay, "明日", "今日")
+	t := time.Now()
+	if params.IsPreviousDay {
+		t = t.AddDate(0, 0, 1)
+	}
+
 	req := &messaging_api.PushMessageRequest{
 		To: os.Getenv("LINE_GROUP_ID"),
 		Messages: []messaging_api.MessageInterface{
 			&messaging_api.TextMessage{
-				Text: fmt.Sprintf("今日 (%s) は %s の収集日です", weekdayToJa(time.Now().In(jst).Format("1/2・Mon")), params.GarbageType),
+				Text: fmt.Sprintf("%s (%s) は %s の収集日です", d, weekdayToJa(t.In(jst).Format("1/2・Mon")), params.GarbageType),
 			},
 		},
 	}
--- notifier/go.mod
+++ notifier/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/GoogleCloudPlatform/functions-framework-go v1.9.2
 	github.com/google/uuid v1.6.0
 	github.com/line/line-bot-sdk-go/v8 v8.13.1
+	github.com/samber/lo v1.52.0
 )
 
 require (
@@ -17,5 +18,6 @@ require (
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.10.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 	golang.org/x/time v0.11.0 // indirect
 )
